5d4b56f033de47834b9e861a449e9933f4161c5b
Elodie Jance <elodie.jance@gmail.com>
2024-05-17 10:58:56 +0200

    correction chemin bug affichage icone home

--- FrontEnd/src/pages/home/home.jsx
+++ FrontEnd/src/pages/home/home.jsx
@@ -13,17 +13,17 @@ function Home() {
         <section className="features">
           <h2 className="sr-only">Features</h2>
           <FeatureItem
-            icon="./img/icon-chat.png"
+            icon="./image/icon-chat.png"
             title="You are our #1 priority"
             description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
           />
           <FeatureItem
-            icon="./img/icon-money.png"
+            icon="./image/icon-money.png"
             title="More savings means higher rates"
             description="The more you save with us, the higher your interest rate will be!"
           />
           <FeatureItem
-            icon="./img/icon-security.png"
+            icon="./image/icon-security.png"
             title="Security you can trust"
             description="We use top of the line encryption to make sure your data and money is always safe."
           />
