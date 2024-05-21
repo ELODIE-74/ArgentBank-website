b34798e1bae0d6a150207be2ed51315bba257d00
Elodie Jance <elodie.jance@gmail.com>
2024-05-21 14:52:55 +0200

    essai store

--- FrontEnd/src/store.js
+++ FrontEnd/src/store.js
@@ -1,13 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./reducers"; // Remplacez 'rootReducer' par votre rootReducer réel
-import errorReducer from "./errorReducer"; // Importez votre errorReducer s'il est défini dans un autre fichier
+import userReducer from "./reducers/userReducer";
+
+const rootReducer = {
+  user: userReducer,
+};
 
 const store = configureStore({
-  reducer: {
-    root: rootReducer,
-    error: errorReducer, // Assurez-vous d'importer et de définir errorReducer correctement
-  },
-  devTools: true,
+  reducer: rootReducer,
 });
 
 export default store;
