857422fbbe21bc5c0290a29e56e5e206a57c31b1
Elodie Jance <elodie.jance@gmail.com>
2024-05-25 15:55:17 +0200

    essai nouveau header connecté

--- FrontEnd/src/components/navuser/navuser.jsx
+++ FrontEnd/src/components/navuser/navuser.jsx
@@ -1,4 +1,39 @@
 import React from "react";
+import logoConnected from "../../assets/image/logoconnecte.png";
+import "../headeruser/headeruser.css";
+
+function MainNavUser() {
+  return (
+    <nav className="main-nav-connected">
+      <div className="main-nav-left">
+        <a className="main-nav-logo" href="/">
+          <img
+            className="main-nav-logo-image"
+            src={logoConnected}
+            alt="Argent Bank Logo"
+          />
+        </a>
+        <h1 className="main-nav-title">Argent Bank</h1>
+      </div>
+      <div className="main-nav-right">
+        <a className="main-nav-item" href="/user">
+          <i className="fa fa-user-circle"></i>
+          Ben Hong
+        </a>
+        <a className="main-nav-item" href="/settings">
+          <i className="fas fa-cog"></i>
+        </a>
+        <a className="main-nav-item" href="/">
+          <i className="fas fa-power-off"></i>
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default MainNavUser;
+
+/*import React from "react";
 
 function MainNavUser() {
   return (
@@ -23,4 +58,4 @@ function MainNavUser() {
     </nav>
   );
 }
-export default MainNavUser;
+export default MainNavUser;*/
