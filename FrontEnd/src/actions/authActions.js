b435352bea9ac169d5f496c0630a28a06c6dd649
Elodie Jance <elodie.jance@gmail.com>
2024-06-12 10:57:23 +0200

    changement code action+signinform pour le profil

--- FrontEnd/src/actions/authActions.js
+++ FrontEnd/src/actions/authActions.js
@@ -4,56 +4,69 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 export const login = createAsyncThunk(
   "auth/login",
   async ({ email, password }) => {
-    try {
-      const response = await fetch("http://localhost:3001/api/v1/user/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      });
+    //appel API pour récupérer le token lors de l'authentification par le biais de l'email et du mot de passe
+    const response = await fetch("http://localhost:3001/api/v1/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
 
-      if (!response.ok) {
-        const error = await response.json();
-        throw new Error(error.message);
-      }
-
-      const data = await response.json();
-      const token = data.token;
-      localStorage.setItem("accessToken", token);
-      return data;
-    } catch (error) {
-      throw new Error(error.message);
+    if (!response.ok) {
+      throw new Error("Erreur lors de la connexion");
     }
+
+    const data = await response.json();
+    //affichage du jeton d'authentification (token)
+    const token = data.token;
+    //vérification du jeton d'authentification
+    console.log("accessToken", token);
+    localStorage.setItem("accessToken", token); // stockage du jeton
+    return data;
   }
 );
-
+//appel api pour récupérer les données du profil utilisateur
 export const fetchUserProfile = createAsyncThunk(
   "auth/fetchUserProfile",
   async () => {
+    //récupération du jeton d'authentification
     const accessToken = localStorage.getItem("accessToken");
+    console.log("accessToken", accessToken);
+    //si jeton pas trouvé (token)
     if (!accessToken) {
       throw new Error("Aucun jeton d'authentification trouvé");
     }
-
+    // appel api des données utlisateur
     try {
-      const response = await fetch("http://localhost:3001/api/v1/user/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-
+      const response = await fetch(
+        "http://localhost:3001/api/v1/user/profile",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      // si requête aboutit pas, renvoie un message d'erreur
       if (!response.ok) {
         const error = await response.json();
         throw new Error(error.message);
       }
-
-      const data = await response.json();
-      return data;
+      // récupération et utlisation des propriétés dans différents composants
+      const userData = await response.json();
+      const { email, firstName, lastName, userName } = userData; // Extraction des données spécifiques du profil utilisateur
+      // variable qui contient les propriétés de l'utlisateur
+      const userProfileData = {
+        email,
+        firstName,
+        lastName,
+        userName,
+      };
+      return userProfileData; // Renvoie les données du profil utilisateur
     } catch (error) {
-      throw new Error(error.message);
+      throw new Error(error.message); //si mauvais renvoie des données
     }
   }
 );
@@ -101,57 +114,6 @@ export const fetchUserProfile = createAsyncThunk(
     }
 
     const data = await response.json();
-    return data;
-  }
-);*/
-/*export const fetchUserProfile = createAsyncThunk(
-  "auth/fetchUserProfile",
-  async (_, { getState }) => {
-    const { accessToken, id } = getState().auth;
-
-    const response = await fetch("http://localhost:3001/api/v1/user/profile", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      body: JSON.stringify({ id }),
-    });
-
-    if (!response.ok) {
-      // Si la réponse n'est pas ok, nous lançons une erreur
-      throw new Error(
-        "Erreur lors de la récupération du profil de l'utilisateur"
-      );
-    }
-
-    const data = await response.json();
-    // Retourne les données contenant le nom d'utilisateur (username)
-    return data;
-  }
-);*/
-
-/*export const fetchUserProfile = createAsyncThunk(
-  "auth/fetchUserProfile",
-  async (_, { getState }) => {
-    const { accessToken, userName } = getState().auth;
-
-    const response = await fetch("http://localhost:3001/api/v1/user/profile", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      body: JSON.stringify({ userName }),
-    });
-
-    if (!response.ok) {
-      // Si la réponse n'est pas ok, nous lançons une erreur
-      throw new Error(
-        "Erreur lors de la récupération du profil de l'utilisateur"
-      );
-    }
-
-    const data = await response.json();
-    // Retourne les données contenant le nom d'utilisateur (username)
     return data;
   }
 );*/
--- FrontEnd/src/components/signinfom/signinform.jsx
+++ FrontEnd/src/components/signinfom/signinform.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { fetchUserProfile, login } from "../../actions/authActions";
-
+//import{userProfilData} from "../../actions/authActions";
 const SignInForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -11,28 +11,24 @@ const SignInForm = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleLogin = async (email, password) => {
-    try {
-      dispatch(login({ email, password }));
-      dispatch(fetchUserProfile());
-      // Rediriger vers la page de l'utilisateur
-      navigate("/user");
-    } catch (error) {
-      console.error(error.message);
-      navigate("/error404"); // Gérer les erreurs de connexion
-    }
-  };
-  /**const userData = {
-email,
-password,
-firstName,
-lastName,
-username
-}; */
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    handleLogin(email, password);
+    dispatch(login({ email, password }))
+      .then((data) => {
+        // Récupération du token depuis le payload lors de l'authentification
+        const accessToken = data.payload.token;
+        return dispatch(fetchUserProfile(accessToken));
+      })
+      .then((userProfileData) => {
+        // userData contient les informations du profil de l'utilisateur
+        /**
+         * const userProfildData = data.payload.userData;
+         * return dispatch(fetchUserProfile(userData));  */
+        navigate("/user"); // Redirection vers la page du profil de l'utilisateur
+      })
+      .catch((error) => {
+        navigate("/error404"); // Gérer les erreurs de connexion
+      });
   };
 
   return (
@@ -72,6 +68,30 @@ username
 };
 
 export default SignInForm;
+
+/*const handleLogin = async (email, password) => {
+    try {
+      dispatch(login({ email, password }));
+      dispatch(fetchUserProfile());
+      // Rediriger vers la page de l'utilisateur
+      navigate("/user");
+    } catch (error) {
+      console.error(error.message);
+      navigate("/error404"); // Gérer les erreurs de connexion
+    }
+  };
+  const userData = {
+email,
+password,
+firstName,
+lastName,
+username
+}; 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleLogin(email, password);
+  };*/
 /*import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -112,73 +132,6 @@ function SignInForm() {
           <input
             type="email"
             id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="input-wrapper">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div className="input-remember">
-          <input type="checkbox" id="remember-me" />
-          <label htmlFor="remember-me">Remember me</label>
-        </div>
-        <button type="submit" className="sign-in-button" disabled={loading}>
-          {loading ? "Loading..." : "Sign In"}
-        </button>
-      </form>
-    </section>
-  );
-}
-
-export default SignInForm;*/
-/*import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { fetchUserProfile, login } from "../../actions/authActions";
-
-function SignInForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const { loading, error } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(login({ email, password }))
-      .then((data) => {
-        const accessToken = data.body.token;
-        return dispatch(fetchUserProfile(accessToken));
-      })
-      .then((userProfile) => {
-        // Utilisez directement le userProfile retourné par l'action fetchUserProfile
-        localStorage.setItem("user", JSON.stringify(userProfile));
-        localStorage.setItem("accessToken", accessToken);
-        navigate("/user"); // Redirigez l'utilisateur vers la page d'accueil après une connexion réussie
-      })
-      .catch((error) => {
-        navigate("/error404"); // Gérer les erreurs de connexion
-      });
-  };
-
-  return (
-    <section className="sign-in-content">
-      <i className="fa fa-user-circle sign-in-icon"></i>
-      <h1>Sign In</h1>
-      {error && <div className="error">{error}</div>}
-      <form onSubmit={handleSubmit}>
-        <div className="input-wrapper">
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            id="username"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
