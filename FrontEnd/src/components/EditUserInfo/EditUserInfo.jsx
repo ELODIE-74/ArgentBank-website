68686a0853878270f2cc7886e75fe8f45d628e39
Elodie Jance <elodie.jance@gmail.com>
2024-06-23 11:37:24 +0200

    récupération infos user, changement du userName + mise a jour + gestion de la déconnexion

--- FrontEnd/src/components/EditUserInfo/EditUserInfo.jsx
+++ FrontEnd/src/components/EditUserInfo/EditUserInfo.jsx
@@ -2,33 +2,232 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateUsername } from "../../actions/authActions";
 import "../EditUserInfo/EditUserinfo.css";
+const EditUserInfo = () => {
+  const { userProfile, accessToken } = useSelector((state) => state.auth);
+  const { firstName, lastName, userName } = userProfile;
+  const dispatch = useDispatch();
+  const [message, setMessage] = useState("");
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [username, setUsername] = useState(userName);
+  const [initialUsername, setInitialUsername] = useState(userName);
+  const [isLoading, setIsLoading] = useState(false);
+
+  /**est appelée lorsque l'utilisateur clique sur le bouton "Edit User Info".
+   * Elle affiche le formulaire d'édition en mettant isFormVisible à true et
+   * initialise username et initialUsername avec la valeur actuelle du userName. */
+  const handleEditName = () => {
+    setIsFormVisible(true);
+    setUsername(userName);
+    setInitialUsername(userName);
+  };
+
+  /**appelée lorsque l'utilisateur modifie la valeur du champ username.
+   * Elle met à jour la valeur de username en conséquence. */
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  //Met à jour la valeur de initialUsername avec la nouvelle valeur de username
+  const handleSave = async () => {
+    try {
+      setIsLoading(true);
+      await dispatch(updateUsername({ accessToken, userName: username }));
+      setMessage("Informations enregistrées avec succès !");
+      setIsFormVisible(false);
+      setInitialUsername(username);
+    } catch (error) {
+      setMessage("Une erreur est survenue lors de l'enregistrement.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  //Met à jour le message pour indiquer que les modifications ont été annulées
+  const handleCancel = () => {
+    setMessage("Modifications annulées.");
+    setIsFormVisible(false);
+    setUsername(initialUsername);
+  };
+
+  //retourne le rendu conditionnel du bouton "Edit User Info" ou du formulaire d'édition en fonction de la valeur de isFormVisible.
+  return (
+    <div>
+      {!isFormVisible && (
+        <button onClick={handleEditName}>Edit User Info</button>
+      )}
+      {isFormVisible && (
+        <form className="form-group edit-user-info">
+          <div>
+            <label htmlFor="firstName">First Name:</label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+            />
+          </div>
+          <div>
+            <label htmlFor="lastName">Last Name:</label>
+            <input type="text" id="lastName" name="lastName" value={lastName} />
+          </div>
+          <div>
+            <label>Username:</label>
+            <input
+              type="text"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </div>
+
+          <div className="buttons">
+            <button
+              type="button"
+              className="save-btn"
+              onClick={handleSave}
+              disabled={isLoading}
+            >
+              {isLoading ? "Saving..." : "Save"}
+            </button>
+            <button
+              type="button"
+              className="cancel-btn"
+              onClick={handleCancel}
+              disabled={isLoading}
+            >
+              Cancel
+            </button>
+          </div>
+          {message && <div>{message}</div>}
+        </form>
+      )}
+    </div>
+  );
+};
+export default EditUserInfo;
+/*import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { updateUsername } from "../../actions/authActions";
+import "../EditUserInfo/EditUserinfo.css";
+const EditUserInfo = () => {
+  const { userProfile, accessToken } = useSelector((state) => state.auth);
+  const { firstName, lastName, userName } = userProfile;
+  const dispatch = useDispatch();
+  const [message, setMessage] = useState("");
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [username, setUsername] = useState(userName);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleEditName = () => {
+    setIsFormVisible(true);
+    setUsername(userName);
+  };
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleSave = async () => {
+    try {
+      setIsLoading(true);
+      await dispatch(updateUsername({ accessToken, userName: username }));
+      setMessage("Informations enregistrées avec succès !");
+      setIsFormVisible(false);
+    } catch (error) {
+      setMessage("Une erreur est survenue lors de l'enregistrement.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleCancel = () => {
+    setMessage("Modifications annulées.");
+    setIsFormVisible(false);
+  };
+
+  return (
+    <div>
+      {!isFormVisible && (
+        <button onClick={handleEditName}>Edit User Info</button>
+      )}
+      {isFormVisible && (
+        <form className="form-group edit-user-info">
+          <div>
+            <label htmlFor="firstName">First Name:</label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+            />
+          </div>
+          <div>
+            <label htmlFor="lastName">Last Name:</label>
+            <input type="text" id="lastName" name="lastName" value={lastName} />
+          </div>
+          <div>
+            <label htmlFor="userName">UserName:</label>
+            <input
+              type="text"
+              id="userName"
+              name="userName"
+              value={userName}
+              onChange={handleUsernameChange}
+            />
+          </div>
+          <div className="buttons">
+            <button
+              type="button"
+              className="save-btn"
+              onClick={handleSave}
+              disabled={isLoading}
+            >
+              {isLoading ? "Saving..." : "Save"}
+            </button>
+            <button
+              type="button"
+              className="cancel-btn"
+              onClick={handleCancel}
+              disabled={isLoading}
+            >
+              Cancel
+            </button>
+          </div>
+          {message && <div>{message}</div>}
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default EditUserInfo;*/
+/*import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { updateUsername } from "../../actions/authActions";
+import "../EditUserInfo/EditUserinfo.css";
 
 const EditUserInfo = () => {
   const { userProfile, accessToken } = useSelector((state) => state.auth);
-  console.log(userProfile);
-  console.log(accessToken);
-  /* const dispatch = useDispatch();
-  const { userProfile, auth } = useSelector((state) => state);
-  const accessToken = auth.accessToken;
-  const [userName, setUsername] = useState("");
+  const { firstName, lastName, userName } = userProfile;
+  const dispatch = useDispatch();
   const [message, setMessage] = useState("");
   const [isFormVisible, setIsFormVisible] = useState(false);
-
+  const [username, setUsername] = useState(userName);
+  //console.log(userProfile);
+  //console.log(accessToken);
   //affichage du formulaire
-  const handleEditName = () => {
+  function handleEditName() {
     setIsFormVisible(true);
-    setUsername(userProfile?.userName || "");
-  };
+    setUsername(userName);
+  }
 
   //changelement du userName, nouvelle valeur
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
   };
 
-  //sauvegarde et envoie de la nouvelle valeur
   const handleSave = async () => {
     try {
-      await dispatch(updateUsername(accessToken, userName));
+      await dispatch(updateUsername({ accessToken, userName }));
       setMessage("Informations enregistrées avec succès !");
       setIsFormVisible(false);
     } catch (error) {
@@ -36,42 +235,24 @@ const EditUserInfo = () => {
     }
   };
 
-  //annulation des données
   const handleCancel = () => {
-    setUsername(userProfile.userName);
     setMessage("Modifications annulées.");
     setIsFormVisible(false);
   };
-  console.log(userProfile);*/
-  //rendu du composant
+
   return (
-    <div></div>
-    /*
     <form
       className={`form-group edit-user-info ${
         isFormVisible ? "form-visible" : ""
       }`}
     >
-
       <div>
         <label htmlFor="firstName">First Name:</label>
-        <input
-          type="text"
-          id="firstName"
-          name="firstName"
-          value={userProfile?.firstName || ""}
-          disabled={status === "loading"}
-        />
+        <input type="text" id="firstName" name="firstName" value={firstName} />
       </div>
       <div>
         <label htmlFor="lastName">Last Name:</label>
-        <input
-          type="text"
-          id="lastName"
-          name="lastName"
-          value={userProfile?.lastName || ""}
-          disabled={status === "loading"}
-        />
+        <input type="text" id="lastName" name="lastName" value={lastName} />
       </div>
       <div>
         <label htmlFor="userName">UserName:</label>
@@ -81,7 +262,6 @@ const EditUserInfo = () => {
           name="userName"
           value={userName}
           onChange={handleUsernameChange}
-          disabled={status === "loading"}
         />
       </div>
       <div className="buttons">
@@ -93,8 +273,8 @@ const EditUserInfo = () => {
         </button>
       </div>
       {message && <div>{message}</div>}
-    </form>*/
+    </form>
   );
 };
 
-export default EditUserInfo;
+export default EditUserInfo;*/
